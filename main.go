ac734c143f949bb8d0edb720f4100c4ddf49a6a8
Jason Weber <weberjas@gmail.com>
2021-07-22 14:07:32 -0600

    Changed approach to just regexp the whole file contents at once

--- main.go
+++ main.go
@@ -3,11 +3,38 @@ package main
 import (
 	"fmt"
 	"io/fs"
+	"io/ioutil"
+	"log"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
+var projectContents = make(map[string]outlinePackage)
+
+// define regular expressions for elements of interest
+var packageRegex = regexp.MustCompile(`^package (\S+)`)
+var functionRegex = regexp.MustCompile(`^func \S+`)
+
+type outlinePackage struct {
+	name      string
+	functions map[string]outlineFunc
+	structs   map[string]outlineStruct
+}
+
+type outlineFunc struct {
+	name    string
+	calls   []string // TODO: this is going to be a hard one to figure out, do we just want to reference local functions?
+	returns string
+}
+
+type outlineStruct struct {
+	name  string
+	funcs []outlineFunc
+}
+
 func main() {
+
 	err := filepath.WalkDir(".", func(path string, info fs.DirEntry, err error) error {
 
 		if err != nil {
@@ -15,7 +42,17 @@ func main() {
 		}
 
 		if strings.HasSuffix(info.Name(), ".go") {
-			fmt.Println(info.Name())
+
+			fileContents, err := ioutil.ReadFile(path)
+			if err != nil {
+				log.Fatalf("Failed to read file: %s", path)
+			}
+
+			// extract the package name
+			packageName := packageRegex.FindStringSubmatch(string(fileContents))[1]
+
+			fmt.Printf("Package Name: %s\n", packageName)
+
 		}
 		return nil
 	})
